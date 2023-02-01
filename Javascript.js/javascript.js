63b279bd4bd0a3785038a6fc592b508fa2f6d590
Clécio Sousa <54221604+cleciosousa@users.noreply.github.com>
2023-02-01 20:57:18 -0300

    Create javascript.js

---
+++ Javascript.js/javascript.js
@@ -0,0 +1,15 @@
+
+let menuIcon = document.querySelector('#menu-icon')
+let nav = document.querySelector('#nav')
+let menuOpen = document.querySelector('.icon-click')
+let iconClose = document.querySelector('#icon-close')
+
+menuIcon.addEventListener('click',()=>{
+    menuOpen.style.display = 'block'
+    nav.style.display = 'none'
+})
+
+iconClose.addEventListener('click',()=>{
+    menuOpen.style.display = 'none'
+    nav.style.display = 'flex'
+})
